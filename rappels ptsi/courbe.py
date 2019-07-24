af7202ce1c51b801127dade0620ea96c2c56fa6a
Eric CORTIAL <flateric94@gmail.com>
2019-07-24 11:14:54 +0200

    commit du 24/07/2019

---
+++ rappels ptsi/courbe.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import math as ma
+
+N=1000
+Listex=[k*4*ma.pi/N for k in range(N+1)]
+Listeycos=[ma.cos(x) for x in Listex]
+Listeysin=[ma.sin(x) for x in Listex]
+
+plt.plot(Listex,Listeycos,label='y=cos(x)')
+plt.plot(Listex,Listeysin,label='y=sin(x)')
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title("Graphique_test")
+plt.legend(loc=3)
+plt.show()
+
+plt.figure()
+
+plt.subplot(2,1,1) #plt.subplot(n,p,k)
+#la fenetre graphique est découpée en n*p figures (n:ligne,p:colonne) et le code concerne la kieme de ces figures
+plt.plot(Listex,Listeycos,label='y=cos(x)')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend(loc=3)
+
+plt.subplot(2,1,2)    
+plt.plot(Listex,Listeysin,label='y=sin(x)')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend(loc=3)
+
+plt.show()
--- tp4/exercice1.py
+++ tp4/exercice1.py
@@ -8,4 +8,34 @@ def impair_iter(n):
         resultat=resultat+(depart+2*i) 
     return(resultat)
 
-print(impair_iter(3))
+def impair_iter_detail(n):
+    i=0
+    depart=1
+    resultat=0
+    detail=""
+    j=0
+    if n==0:
+        return(0)
+    for i in range(n):
+        resultat=resultat+(depart+2*i)
+        detail = detail + str(depart+2*i)
+        if i!=n-1:
+            detail = detail + " + "
+        else:
+            detail = detail + " = "
+    print(detail + str(resultat))
+    return(resultat)
+
+def impair_recu(n):
+    if n==1:
+        return(1)
+    else:
+        return(n+(n-1)+impair_recu(n-1))
+
+
+
+
+print(impair_iter(8))
+print(impair_iter_detail(8))
+print(impair_recu(8))
+
