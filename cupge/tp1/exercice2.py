b978727a00a76b5d856af4b8815299743ae2e456
Eric CORTIAL <flateric94@gmail.com>
2020-01-23 23:40:36 +0100

    exo2 fiinii

--- cupge/tp1/exercice2.py
+++ cupge/tp1/exercice2.py
@@ -13,25 +13,29 @@ def testperm(a):
         
     return("c'est bon")
 
-print(testperm([1,2,3]))
-print(testperm([6,4,3,1,2,5]))
-print(testperm([1,1,0]))
-print(testperm([2,0,1,2]))
-print(testperm([0,2,3,6,3]))
-print(testperm([-1,3,8,4,0,78]))
+# print(testperm([1,2,3]))
+# print(testperm([6,4,3,1,2,5]))
+# print(testperm([1,1,0]))
+# print(testperm([2,0,1,2]))
+# print(testperm([0,2,3,6,3]))
+# print(testperm([-1,3,8,4,0,78]))
 
-#question2)
+#question2) on introduit un tableau l dans lequel on va se souvenir des éléments déjà utilisés dans la construction d'une permutation
 
-# def testpermoptial(a):
-#     n=len(a)
-#     l=[0]*n
-#     repere=a[i]
-#     for i in range(n):
-        
+def testpermoptial(a):
+    n=len(a)
+    l=[0]*n
+    for i in range(n):
+        # print(a[i])
+        if a[i]<0 or a[i]>len(a):
+            return("erreur")
+        if l[a[i]-1]==0:
+           l[a[i]-1]=1
+        else:
+            return("erreur")
+    return("correct")
 
-
-    
-    
-
-
-# print(testpermoptial([1,2,3,4]))
+     
+print(testpermoptial([1,2,3,4]))
+print(testpermoptial([2,3,1,2]))
+print(testpermoptial([1,2,4,45,5]))
