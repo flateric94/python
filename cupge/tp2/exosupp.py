7d254c16aab98af4393437ce71366656db3cb480
Eric CORTIAL <flateric94@gmail.com>
2020-02-05 10:20:26 +0100

    morpion terminé mais améliorations possibles

--- cupge/tp2/exosupp.py
+++ cupge/tp2/exosupp.py
@@ -1,15 +1,29 @@
 #triangle de pascal
 
-def pascal(N):                  #N:numero de la ligne avec premiere ligne à 1
-    liste=[[1],[1,1]]         #je definie la 1ere et la 2eme ligne
-    print(liste[0])
-    print(liste[1])
-    for i in range(2,N):        #je m'occupe des nouvelles lignes
-        liste.append([1])       #chaque ligne commence par un 1
-        for j in range(1,i):    #je m'occupe des nouveaux termes, en sachant qu'il y a un 1 en debut de ligne
-            liste[i].append(liste[i-1][j-1] + liste[i-1][j])    #calcul du j^ieme terme
-        liste[i].append(1)      #chaque ligne finie par un 1
-        print(liste[i])
+def pascal(N):     
+    #attention! N commence à 1            
+    #je definie la 1ere et la 2eme ligne
+    liste=[[1],[1,1]]         
+    # print(liste[0])
+    # print(liste[1])
+    if N==1:
+        return([1])
+    if N==2:
+        return([1,1])
+    if N==3:
+        return([1,2,1])
+    #je m'occupe des nouvelles lignes
+    for i in range(2,N): 
+        #chaque ligne commence par un 1    
+        liste.append([1])      
+        #je m'occupe des nouveaux termes, en sachant qu'il y a un 1 en debut de ligne
+        for j in range(1,i):  
+            #calcul du j^ieme terme  
+            liste[i].append(liste[i-1][j-1] + liste[i-1][j])  
+        #chaque ligne finie par un 1  
+        liste[i].append(1)     
     return(liste[i])
 
-print(pascal(3))    #fonctionne pour N>2
+for i in range(1,9):
+    print(pascal(i)) 
+   
