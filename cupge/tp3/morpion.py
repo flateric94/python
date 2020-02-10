3b174761ba5dc8f36b6a0022fa5b7294c631aead
Eric CORTIAL <flateric94@gmail.com>
2020-02-10 12:16:31 +0100

    morpion adapté (ordi vs ordi ou hum vs hum apres observations du programme de gnk

--- cupge/tp3/morpion.py
+++ cupge/tp3/morpion.py
@@ -8,172 +8,166 @@ en argument le nom du joueur qui commence ("humain" ou "ordinateur").
 
 import random as rd
 
+compteJoueur = 0 # va servir a affecter un numero derriere chaque joueur pour les differencier
+def incremente_joueur():
+    global compteJoueur # explique qu'on se referre à la variable globale, sinon on ne peut la modifier depuis une methode
+    compteJoueur += 1
+
+class joueur():
+    """Classe définissant les informations relatives aux joueurs par : 
+    -   le type du joueur (ordinateur ou humain)
+    -   le type de sigle utilisé (x ou o)
+
+    --> va simplifier le problème pour l'attribution des joueurs, permettant de faire joueur 
+    humain/humain, ordi/ordi
+    """
+    def __init__(self, typedejoueur, sigle):
+        if typedejoueur not in ["humain" , "ordinateur"]:
+            print("attribuer un nom correct (humain/ordinateur)")
+            return
+        else:
+            incremente_joueur()
+            self.typedejoueur = typedejoueur
+            self.sigle = sigle
+            self.nom = typedejoueur + str(compteJoueur)
+
+
 class morpion:   #défition de notre classe morpion
     """Classe définissant l'état initial du plateau de jeu par :
     -   le type de plateau, ici 3*3
     -   le nom de l'utilisateur"""
 
-    def __init__(self, nom): # Notre méthode constructeur
+    def __init__(self, premier_joueur, deuxieme_joueur): # Notre méthode constructeur
         self.plateau=[[0,0,0],[0,0,0],[0,0,0]]
-        self.nom=nom
-        if nom != "utilisateur" and nom != "ordinateur":
-            print("error")
-        
-           
+        self.joueurs = [joueur(premier_joueur, 1), joueur(deuxieme_joueur, -1)]
 
-""" question2)
-écrire une fonction afficher_plateau() qui permet d'afficher le plateau ligne par ligne avec des 
-croix à la place des -1, des ronds à la place des 1, et rien à la place des 0.
-"""
-def afficher_plateau(self):
-    for i in range(3):
-        ligne = []
-        for j in range(3):
-            if self.plateau[i][j]==1:
-                ligne.append("o")
-            elif self.plateau[i][j]==-1:
-                ligne.append("x")
-            elif self.plateau[i][j]==0:
-                ligne.append(" ")
-        print(ligne)
+  
+    def afficher_plateau(self):
+        """ question2)
+        écrire une fonction afficher_plateau() qui permet d'afficher le plateau ligne par ligne avec des 
+        croix à la place des -1, des ronds à la place des 1, et rien à la place des 0.
+        """
+        for i in range(3):
+            ligne = []
+            for j in range(3):
+                if self.plateau[i][j]==1:
+                    ligne.append("o")
+                elif self.plateau[i][j]==-1:
+                    ligne.append("x")
+                elif self.plateau[i][j]==0:
+                    ligne.append(" ")
+            print(ligne)
 
-""" question3)
-écrire une fonction joueur_humain() qui vous permet via input() de dire où vous voulez jouer
-en numéro de ligne et de colonne. Cette fonction doit gérer le fait que vous n'avez le droit de 
-jouer que dans une case vide.
-"""
+    def jouer_humain(self, sigle):
+        """ question3)
+        écrire une fonction jouer_humain() qui vous permet via input() de dire où vous voulez jouer
+        en numéro de ligne et de colonne. Cette fonction doit gérer le fait que vous n'avez le droit de 
+        jouer que dans une case vide.
+        """
+        # choix de la ligne et vérification de son existence
+        print("choisissez la ligne : ")
+        ligne=int(input())
+        if ligne>2 or ligne<0:
+            print("les lignes existantes sont entres 0 et 2")
+            self.joueur_humain(sigle)
+        # choix de la colonne et vérification de son existence
+        print("choisissez la colonne : ")
+        colonne=int(input())
+        if colonne>2 or colonne<0:
+            print("les colonnes existantes sont entres 0 et 2")
+            self.joueur_humain(sigle)
+        #vérification case vide
+        if self.plateau[ligne][colonne] != 0:
+            print("joue dans une case vide")
+            self.joueur_humain(sigle)
+        #la case se remplit
+        else:
+            self.plateau[ligne][colonne]=1
+            self.afficher_plateau()
 
-def joueur_humain(self):
-    # choix de la ligne et vérification de son existence
-    print("choisissez la ligne : ")
-    ligne=int(input())
-    if ligne>2 or ligne<0:
-        print("les lignes existantes sont entres 0 et 2")
-        joueur_humain(self)
-    # choix de la colonne et vérification de son existence
-    print("choisissez la colonne : ")
-    colonne=int(input())
-    if colonne>2 or colonne<0:
-        print("les colonnes existantes sont entres 0 et 2")
-        joueur_humain(self)
-    #vérification case vide
-    if self.plateau[ligne][colonne] != 0:
-        print("joue dans une case vide")
-        joueur_humain(self)
-    #la case se remplit
-    else:
-        self.plateau[ligne][colonne]=1
-        print(afficher_plateau(self))
-
-"""question4)
-écrire une fonction trouver_cases_vides() qui renvoie une liste des indices (lignes, colonnes)
-des cases vides.
-"""
-
-def trouver_cases_vides(self):
-    liste_indice_cases_vides=[]
-    for i in range(3):
-        for j in range(3):
-            if self.plateau[i][j]==0:
-                liste_indice_cases_vides.append((i,j))
-    return(liste_indice_cases_vides)
-
-"""question5)
-écrire une fonction joueur_aleatoire() qui joue aléatoirement dans une des cases vides.
-"""
-def joueur_aleatoire(self):
-    L=trouver_cases_vides(self)
-    (i,j)=rd.choice(L)
-    self.plateau[i][j]=-1
-    print(afficher_plateau(self))
-
-
-"""question6)
-ecrire une fonction partie_gagnee() qui renvoie le joueur gagnant : None, "humain", 
-ou "ordinateur", en fonction des règles du jeu du morpion.
-"""
-
-def partie_gagnee(self):
-    resultat = 0
-    # afficher_plateau(self)
-    if self.plateau[0][0]==1 and self.plateau[0][1]==1 and self.plateau[0][2]==1:
-        resultat=1
-    if self.plateau[1][0]==1 and self.plateau[1][1]==1 and self.plateau[1][2]==1:
-        resultat=1
-    if self.plateau[2][0]==1 and self.plateau[2][1]==1 and self.plateau[2][2]==1:
-        resultat=1
-    if self.plateau[0][0]==1 and self.plateau[1][0]==1 and self.plateau[2][0]==1:
-        resultat=1
-    if self.plateau[0][1]==1 and self.plateau[1][1]==1 and self.plateau[2][1]==1:
-        resultat=1
-    if self.plateau[0][2]==1 and self.plateau[1][2]==1 and self.plateau[2][2]==1:
-        resultat=1
-    if self.plateau[0][0]==1 and self.plateau[1][1]==1 and self.plateau[2][2]==1:
-        resultat=1
-    if self.plateau[0][2]==1 and self.plateau[1][1]==1 and self.plateau[2][0]==1:
-        resultat=1
-    else:
-        if self.plateau[0][0]==-1 and self.plateau[0][1]==-1 and self.plateau[0][2]==-1:
-            resultat=2
-        if self.plateau[1][0]==-1 and self.plateau[1][1]==-1 and self.plateau[1][2]==-1:
-            resultat=2
-        if self.plateau[2][0]==-1 and self.plateau[2][1]==-1 and self.plateau[2][2]==-1:
-            resultat=2
-        if self.plateau[0][0]==-1 and self.plateau[1][0]==-1 and self.plateau[2][0]==-1:
-            resultat=2
-        if self.plateau[0][1]==-1 and self.plateau[1][1]==-1 and self.plateau[2][1]==-1:
-            resultat=2
-        if self.plateau[0][2]==-1 and self.plateau[1][2]==-1 and self.plateau[2][2]==-1:
-            resultat=2
-        if self.plateau[0][0]==-1 and self.plateau[1][1]==-1 and self.plateau[2][2]==-1:
-            resultat=2
-        if self.plateau[0][2]==-1 and self.plateau[1][1]==-1 and self.plateau[2][0]==-1:
-            resultat=2
-    if resultat==1:
-        return("humain")
-    if resultat==2:
-        return("ordinateur")
-    else:
-        return None
-
-
-"""question8)
-mettre tout cela ensemble via une fonction jouer() qui permet effectivement de jouer contre l'ordinateur.
-Et battez-le : c'est très facile contre un joueur aléatoire.
-Idée : il faut une boucle while qui ne se termine que si partie gagnée ou nulle, et trouver d'alterner
-à qui c'est le tour de jouer.
-"""
-
-def jouer(self):
-    afficher_plateau(self)
-    print("quel joueur commence ? : (U : Utilisateur / O : Ordinateur)")
-    joueur_qui_commence=str(input())
-    if joueur_qui_commence != "U" and joueur_qui_commence != "O":
-        return("veuillez mettre un nom correct")
-    print("quel autre joueur joue ? : (U : Utilisateur / O : Ordinateur)")
-    autre_joueur=str(input())
-    if autre_joueur != "U" and autre_joueur != "O":
-        return("veuillez mettre un nom correct")
-    while partie_gagnee(self) != "humain" or partie_gagnee(self) != "ordinateur" or partie_gagnee(self) != None:
-        for i in range(9):
-            if (i%2==0):
-                print(joueur_qui_commence)
-                if joueur_qui_commence == "U":
-                    joueur_humain(self)
-                else:
-                    joueur_aleatoire(self)   
-            if (i%2==1):
-                print(autre_joueur)
-                if autre_joueur == "U":
-                    joueur_humain(self)
-                else:
-                    joueur_aleatoire(self)
-            if partie_gagnee(self)== "humain" or partie_gagnee(self)== "ordinateur":
-                print(partie_gagnee(self))
-                print("fin de partie")
-                return
-        break
+    def trouver_cases_vides(self):
+        #  """question4)
+        # écrire une fonction trouver_cases_vides() qui renvoie une liste des indices (lignes, colonnes)
+        # des cases vides.
+        # """
+        liste_indice_cases_vides=[]
+        for i in range(3):
+            for j in range(3):
+                if self.plateau[i][j]==0:
+                    liste_indice_cases_vides.append((i,j))
+        return(liste_indice_cases_vides)
     
+    def jouer_aleatoire(self, sigle):
+        """question5)
+        écrire une fonction jouer_aleatoire() qui joue aléatoirement dans une des cases vides.
+        """
+        L=self.trouver_cases_vides()
+        (i,j)=rd.choice(L)
+        self.plateau[i][j]=sigle
+        self.afficher_plateau()
+
+    def jouer_uncoup(self, indiceJoueur):
+        print(self.joueurs[indiceJoueur].nom + " joue:")
+        if self.joueurs[indiceJoueur].typedejoueur == "humain":
+            self.jouer_humain(self.joueurs[indiceJoueur].sigle)
+        else:
+            self.jouer_aleatoire(self.joueurs[indiceJoueur].sigle)
+    
+    def partie_gagnee(self):
+        """question6)
+        ecrire une fonction partie_gagnee() qui renvoie le joueur gagnant : None, "humain", 
+        ou "ordinateur", en fonction des règles du jeu du morpion.
+        """
+        resultat = 99
+        if self.plateau[0][0]==1 and self.plateau[0][1]==1 and self.plateau[0][2]==1:
+            resultat=0
+        if self.plateau[1][0]==1 and self.plateau[1][1]==1 and self.plateau[1][2]==1:
+            resultat=0
+        if self.plateau[2][0]==1 and self.plateau[2][1]==1 and self.plateau[2][2]==1:
+            resultat=0
+        if self.plateau[0][0]==1 and self.plateau[1][0]==1 and self.plateau[2][0]==1:
+            resultat=0
+        if self.plateau[0][1]==1 and self.plateau[1][1]==1 and self.plateau[2][1]==1:
+            resultat=0
+        if self.plateau[0][2]==1 and self.plateau[1][2]==1 and self.plateau[2][2]==1:
+            resultat=0
+        if self.plateau[0][0]==1 and self.plateau[1][1]==1 and self.plateau[2][2]==1:
+            resultat=0
+        if self.plateau[0][2]==1 and self.plateau[1][1]==1 and self.plateau[2][0]==1:
+            resultat=0
+        else:
+            if self.plateau[0][0]==-1 and self.plateau[0][1]==-1 and self.plateau[0][2]==-1:
+                resultat=1
+            if self.plateau[1][0]==-1 and self.plateau[1][1]==-1 and self.plateau[1][2]==-1:
+                resultat=1
+            if self.plateau[2][0]==-1 and self.plateau[2][1]==-1 and self.plateau[2][2]==-1:
+                resultat=1
+            if self.plateau[0][0]==-1 and self.plateau[1][0]==-1 and self.plateau[2][0]==-1:
+                resultat=1
+            if self.plateau[0][1]==-1 and self.plateau[1][1]==-1 and self.plateau[2][1]==-1:
+                resultat=1
+            if self.plateau[0][2]==-1 and self.plateau[1][2]==-1 and self.plateau[2][2]==-1:
+                resultat=1
+            if self.plateau[0][0]==-1 and self.plateau[1][1]==-1 and self.plateau[2][2]==-1:
+                resultat=1
+            if self.plateau[0][2]==-1 and self.plateau[1][1]==-1 and self.plateau[2][0]==-1:
+                resultat=1
+        if resultat in [0, 1]:
+            return(resultat)
+        else:
+            return None
+
+    def jouer(self):
+        self.afficher_plateau()
+        while self.partie_gagnee() != 0 or self.partie_gagnee() != 1 or self.partie_gagnee() != None:
+            for i in range(9):
+                self.jouer_uncoup(i%2)
+                if self.partie_gagnee() in [0, 1]:
+                    print(self.joueurs[self.partie_gagnee()].nom + " remporte la partie.")
+                    return
+            print("Match nul, pas de gagnant")
+            return
+
         
-test=morpion("utilisateur")
-jouer(test)
+partie = morpion("humain", "ordinateur")
+partie.jouer()
